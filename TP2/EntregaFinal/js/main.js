9d7514786790c9c862db689e6649f4076edd85bc
FernandaMGonzalez <fermaggonzalez@gmail.com>
2019-09-16 23:59:35 -0300

    Entrega sin finalizar - no pude hacer andar el Drag & Drop

--- TP2/EntregaFinal/js/main.js
+++ TP2/EntregaFinal/js/main.js
@@ -1,37 +1,40 @@
-//document.addEventListener("DOMContentLoaded", function(event) {
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
 
-  let canvas = document.getElementById("myCanvas");
-  let ctx = canvas.getContext("2d");
+let mousePos;
+let click = false;
+let figura = new Polygon(ctx);
+let poligonos = [];
 
-  let mousePos;
-  let click = false;
-  let figura = new Polygon(ctx);
-
-  function getMousePos(canvas, e) {
-    let pos = canvas.getBoundingClientRect();
-    return { // devuelve un objeto
-      x: Math.round(e.clientX - pos.left),
-      y: Math.round(e.clientY - pos.top)
-    }
+function getMousePos(canvas, e) {
+  let pos = canvas.getBoundingClientRect();
+  return { // devuelve un objeto
+    x: Math.round(e.clientX - pos.left),
+    y: Math.round(e.clientY - pos.top)
   }
+}
 
-  canvas.onmousedown = function(e) {
-    click = true;
-    if (click) {
-      mousePos = getMousePos(canvas, e);
-      figura.agregarCirculo(mousePos.x, mousePos.y);
-      let message = 'Se apreto en: ' + mousePos.x + ' - ' + mousePos.y;
-      console.log(message);
-    }
+canvas.onmousedown = function(e) {
+  click = true;
+  if (click) {
+    mousePos = getMousePos(canvas, e);
+    figura.agregarCirculo(mousePos.x, mousePos.y, 10);
+    let message = 'Se apreto en: ' + mousePos.x + ' - ' + mousePos.y;
+    console.log(message);
   }
+}
 
-  function cerrarFigura() {
-    if (figura.circulos.length < 3) {
-      alert('No hay puntos suficientes para unir.')
-    }
-    else {
-      figura.cerrar();
-    }
+function cerrarFigura() {
+  if (figura.circulos.length < 3) {
+    alert('No hay puntos suficientes para unir.')
   }
+  else {
+    figura.cerrar();
+    poligonos.push(figura);
+    figura = new Polygon(ctx);
+  }
+}
 
-//});
+function limpiarCanvas(){
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
